d67343dbb417e77e43f071c0ca4e25cb5653bbc9
pankaj511995 <kpankaj511995@gmail.com>
2023-03-03 23:35:15 +0530

    bodyparser

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express=require('express')
+const bodyparser=require('body-parser')
 const app=express()
+app.use(bodyparser.urlencoded({extended:false}))
 app.use((req,res,next)=>{
     console.log('i am first')
     next()
 })
-app.use((req,res,next)=>{
-    console.log('i am second')
+app.get('/add-product',(req,res,next)=>{
+    res.send(`<form action="/" method="POST">
+    <input type="text" name="prosuct">
+    <input type="number" name="size">
+    <button>send</button> </form>`)
     
 })
-
+app.post('/',(req,res,next)=>{
+    console.log(req.body)
+})
 app.listen(3000)
